27f34ce8fd97e264edd87ea65766a50eb853c789
DeepxHyeon <deepxhyeon@gmail.com>
2020-06-13 20:48:41 +0900

    #2.7 Extracting Companies

--- indeed.py
+++ indeed.py
@@ -23,6 +23,12 @@ def extract_indeed_jobs(last_page):
     results = soup.find_all("div", {"class": "jobsearch-SerpJobCard"})
     for result in results:
         title = result.find("h2", {"class": "title"}).find("a")["title"]
-
-        print(title)
+        company = result.find("span", {"class": "company"})
+        comapny_anchor = company.find("a")
+        if comapny_anchor is not None:
+            company = str(comapny_anchor.string)
+        else:
+            company = str(company.string)
+        company = company.strip()
+        print(title, company)
     return jobs
